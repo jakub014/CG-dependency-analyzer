00658e6ff31f53354932423be3639366bde1ce53
jakub014 <j.h.nguyen@student.tudelft.nl>
2021-04-27 12:51:13 +0200

    Refactored affected method generation, added javadoc

--- analyzer/javacg-opal/src/main/java/CallGraphGenerator.java
+++ analyzer/javacg-opal/src/main/java/CallGraphGenerator.java
@@ -1,8 +1,10 @@
+import com.google.common.collect.BiMap;
 import eu.fasten.analyzer.javacgopal.data.CallGraphConstructor;
 import eu.fasten.analyzer.javacgopal.data.MavenCoordinate;
 import eu.fasten.analyzer.javacgopal.data.PartialCallGraph;
 import eu.fasten.analyzer.javacgopal.data.exceptions.MissingArtifactException;
 import eu.fasten.analyzer.javacgopal.data.exceptions.OPALException;
+import eu.fasten.core.data.DirectedGraph;
 import eu.fasten.core.data.ExtendedRevisionJavaCallGraph;
 import eu.fasten.core.maven.utils.MavenUtilities;
 import eu.fasten.core.merge.LocalMerger;
@@ -11,7 +13,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import scala.reflect.internal.TreeInfo;
+import java.util.stream.Collectors;
 
 public class CallGraphGenerator {
     public static void main(String[] args) throws OPALException, MissingArtifactException {
@@ -19,7 +21,8 @@ public class CallGraphGenerator {
         MavenCoordinate depcoord = new MavenCoordinate("com.googlecode.json-simple", "json-simple", "1.1.1", "jar");
         final var depfile = new MavenCoordinate.MavenResolver().downloadArtifact(depcoord, MavenUtilities.MAVEN_CENTRAL_REPO);
 
-        File file = new File("C:\\Users\\Jakub\\Desktop\\Playing round here\\helloGraphs.jar");
+        //Pass the path as an argument
+        File file = new File(args[0]);
 
         final var opalCG = new CallGraphConstructor(file, "", "CHA");
         final var cg = new PartialCallGraph(opalCG, true);
@@ -50,21 +53,47 @@ public class CallGraphGenerator {
         var mergedDirectedGraph = merger.mergeAllDeps();
         var allUris = merger.getAllUris();
 
+        //TODO understand why amount of methods changes between runs
         System.out.println(rcgDep.mapOfAllMethods().size());
         System.out.println(rcg.mapOfAllMethods().size());
-
+        //TODO understand why there are less nodes in the merged Graph
         System.out.println(mergedDirectedGraph.numNodes());
 
+        var temp = getAffectedMethods(mergedDirectedGraph, allUris, "JSONObject.%3Cinit%3E()", rcg.product);
+    }
 
-        //TODO hardcoded
-        Long vulnerableMethodID = 12l;
+    /**
+     * Returns the id corresponding to the method name given in the allUris map.
+     * @param allUris allUris
+     * @param methodName the methodname
+     * @return the id
+     */
+    public static Long getMethodID(BiMap<Long, String> allUris, String methodName) {
+        for (var x : allUris.entrySet()) {
+            //TODO there might be better ways of getting the local ID
+            if(x.getValue().contains(methodName)) {
+                return x.getKey();
+            }
+        }
+        return -1l;
+    }
 
-
+    /**
+     *
+     * Returns a list of all affected methods in the project.
+     * @param mergedGraph the merged graph of a RCG
+     * @param vulnerableMethodName the vulnerable method in the dependency
+     * @return a list of all affected methods in the project
+     */
+    public static List<String> getAffectedMethods(DirectedGraph mergedGraph, BiMap<Long, String> allUris,  String vulnerableMethodName, String projectName) {
         Set<Long> visited = new HashSet<>();
         Set<Long> toBeVisited = new HashSet<>();
 
+        Long vulnerableMethodID = getMethodID(allUris, vulnerableMethodName);
+
         toBeVisited.add(vulnerableMethodID);
 
+        //TODO add method tracing
         while(!toBeVisited.isEmpty()) {
             Set<Long> toBeVisitedNext = new HashSet<>();
             for(Long method : toBeVisited) {
@@ -73,7 +102,7 @@ public class CallGraphGenerator {
                 if(!visited.contains(method)) {
                     visited.add(method);
                 }
-                mergedDirectedGraph.edgesOf(method).stream().map(x -> x.leftLong()).forEach(x -> {
+                mergedGraph.edgesOf(method).stream().map(x -> x.leftLong()).forEach(x -> {
                     if(!visited.contains(x)) {
                         toBeVisitedNext.add(x);
                     }
@@ -83,7 +112,6 @@ public class CallGraphGenerator {
         }
 
         //TODO to improve performance change way of detecting if method is in correct repo
-        var hey = visited.stream().map(x -> allUris.get(x)).filter(x -> x.contains("helloGraphs")).toArray();
-
+        return  visited.stream().map(x -> allUris.get(x)).filter(x -> x.contains(projectName)).collect(Collectors.toList());
     }
 }
