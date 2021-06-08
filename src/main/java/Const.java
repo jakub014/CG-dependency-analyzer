1e3367e6e14725b790db455593aecf1123e853b8
Niels Mook <niels.mook@nl.gt.com>
2021-06-08 19:36:32 +0200

    Configure run from project 88 onward

--- src/main/java/Const.java
+++ src/main/java/Const.java
@@ -5,5 +5,5 @@ public class Const {
     static final String LOG_FILE_PATH = "analysisResults/analysed-repos.txt";
     static final Long TIMESTAMP_MAY_2020 = 1589454523000L;
     static final Long TIMESTAMP_FEBRUARY_2021 = 1613308674000L;
-    static final String INSPECTING_PROJECTS_PATH = "src/main/resources/report-all-gradle-projects.txt";
+    static final String INSPECTING_PROJECTS_PATH = "src/main/resources/report-all-gradle-projects-tmp.txt";
 }
